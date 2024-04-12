7f479f65f95ec33427dcbc3557847454440a13da
Ruban Gino Singh <rubangino22@gmail.com>
2024-04-12 19:55:27 +0530

    COMPLETE Home.jsx

--- src/assets/Components/Home.jsx
+++ src/assets/Components/Home.jsx
@@ -4,19 +4,21 @@ import { Button } from "@material-tailwind/react"
 
 const Home = () => {
     return (
-        <section className="flex gap-40 justify-center items-center h-[100vh] bg-gradient-to-br from-primaryGradient to-secondaryGradient">
-            <div>
-                <h1 className="text-4xl sm:text-3xl font-bold text-lightColor">Enabling Your Next Digital Leap!</h1>
-                <h1 className="text-4xl sm:text-2xl font-bold text-white mt-4 mb-[-24px]">Elevate Your Reach</h1>
-                <img src={HomeLeft} className="m-0" alt="" />
-                <h5 className="text-lg sm:text-xl lg:text-2xl text-white">With us</h5>
-                <div className="space-x-4 my-4">
-                    <Button variant="filled" color="white">Let's Talk</Button>
-                    <Button variant="outlined" color="white">Project Request</Button>
+        <section id="Home">
+            <div className="flex flex-col sm:flex-row justify-around items-center pt-10 h-[100vh] bg-gradient-to-br from-primaryGradient to-secondaryGradient">
+                <div className="px-6 sm:px-10 md:px-16">
+                    <h1 className="text-xl sm:text-3xl mt-6 sm:mt-0 font-bold text-lightColor">Enabling Your Next Digital Leap!</h1>
+                    <h2 className="text-xl sm:text-2xl font-bold text-white mt-2 sm:mt-3 mb-1 uppercase">Elevate Your Reach</h2>
+                    <img src={HomeLeft} className="sm:px-2 w-full sm:w-80 md:w-full" alt="" />
+                    <h6 className="text-lg sm:text-md lg:text-xl text-white mt-2">With us</h6>
+                    <div className="flex flex-col sm:flex-row mt-4 sm:mt-6 gap-4">
+                        <Button variant="filled" color="white" className="w-full sm:w-auto">Let's Talk</Button>
+                        <Button variant="outlined" color="white" className="w-full sm:w-auto">Project Request</Button>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <img src={HomeImg} alt="" className="w-40 sm:w-56 lg:w-[35rem]" />
+                <div>
+                    <img src={HomeImg} alt="" className="w-56 sm:w-60 lg:w-[32rem] mt-6 sm:mt-0" />
+                </div>
             </div>
         </section>
     );
