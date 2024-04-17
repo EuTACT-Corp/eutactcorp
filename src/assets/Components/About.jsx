776c4ebd9193370e8c9b3b27994297d48a5b3ce0
Ruban Gino Singh <rubangino22@gmail.com>
2024-04-17 22:05:59 +0530

    Fix Responsive

--- src/assets/Components/About.jsx
+++ src/assets/Components/About.jsx
@@ -1,7 +1,7 @@
 const About = () => {
     return (
         <section id="About" className="w-full bg-white">
-            <div className="md:px-8 md:py-8 lg:px-16 lg:py-16 flex flex-col md:flex-row items-center">
+            <div className="p-12 md:px-8 md:py-8 lg:px-16 lg:py-16 flex flex-col md:flex-row items-center">
                 <div className="w-full md:w-1/2 mb-6 md:mb-0 md:mr-8">
                     <h2 className="text-3xl md:text-4xl lg:text-5xl">Who we are?</h2>
                     <p className="text-sm md:text-base lg:text-lg">We are a team of experienced designers and developers, who thrive to build top quartile and unique products. Helping business owners, keep it simple, achieve the goal at a competitive cost and taking ownership with sustainability mindset are our core values.</p>
--- src/assets/Components/Home.jsx
+++ src/assets/Components/Home.jsx
@@ -5,7 +5,7 @@ import { Button } from "@material-tailwind/react"
 const Home = () => {
     return (
         <section id="Home">
-            <div className="flex flex-col sm:flex-row justify-around items-center pt-10 h-[100vh] bg-gradient-to-br from-primaryGradient to-secondaryGradient">
+            <div className="flex flex-col sm:flex-row justify-around items-center pt-24 lg:py-50 h-[100vh] bg-gradient-to-br from-primaryGradient to-secondaryGradient">
                 <div className="px-6 sm:px-10 md:px-16">
                     <h1 className="text-xl sm:text-3xl mt-6 sm:mt-0 font-bold text-lightColor">Enabling Your Next Digital Leap!</h1>
                     <h2 className="text-xl sm:text-2xl font-bold text-white mt-2 sm:mt-3 mb-1 uppercase">Elevate Your Reach</h2>
