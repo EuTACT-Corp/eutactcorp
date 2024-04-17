6f9e29d11379e17c802b02e126ef24d3f98e3caa
Ruban Gino Singh <rubangino22@gmail.com>
2024-04-17 20:47:20 +0530

    ADD Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./assets/Components/Header"
 import Home from "./assets/Components/Home"
+import About from "./assets/Components/About"
 
 function App() {
   return (
     <div>
       <Header />
       <Home />
+      <About />
     </div>
   );
 }
