bb08584a4a803ab7c22159874d3f61d6f77f0ae7
Ruban Gino Singh <rubangino22@gmail.com>
2024-04-18 20:55:52 +0530

    COMPLETE About.jsx with Animation

--- src/assets/Components/About.jsx
+++ src/assets/Components/About.jsx
@@ -1,4 +1,21 @@
+import React, { useEffect, useState } from "react";
+import CountUp from "react-countup";
+
 const About = () => {
+    const [showCountUp, setShowCountUp] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const isScrolled = window.scrollY > 0;
+            setShowCountUp(isScrolled);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     return (
         <section id="About" className="md:px-16 w-full bg-white">
             <div className="p-12 md:px-8 md:py-8 lg:px-16 lg:py-16 flex flex-col md:flex-row items-center">
@@ -8,15 +25,27 @@ const About = () => {
                 </div>
                 <div className="flex flex-wrap justify-center items-center w-full md:w-1/2">
                     <div className="text-center p-5 w-1/2 md:w-1/3">
-                        <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-primaryGradient">100+</h1>
+                        {showCountUp && (
+                            <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-primaryGradient">
+                                <CountUp start={0} end={100} duration={3} suffix='+' />
+                            </h1>
+                        )}
                         <h5 className="text-xs md:text-sm lg:text-base">Successful Project</h5>
                     </div>
                     <div className="text-center p-5 w-1/2 md:w-1/3">
-                        <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-primaryGradient">5+</h1>
+                        {showCountUp && (
+                            <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-primaryGradient">
+                                <CountUp start={0} end={5} duration={3} suffix="+" />
+                            </h1>
+                        )}
                         <h5 className="text-xs md:text-sm lg:text-base">Years of Experience</h5>
                     </div>
                     <div className="text-center p-5 w-1/2 md:w-1/3">
-                        <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-primaryGradient">1200+</h1>
+                        {showCountUp && (
+                            <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-primaryGradient">
+                                <CountUp start={0} end={1200} duration={3} suffix="+" />
+                            </h1>
+                        )}
                         <h5 className="text-xs md:text-sm lg:text-base">Worldwide Clients</h5>
                     </div>
                 </div>
